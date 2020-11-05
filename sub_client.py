a769493a40a321c3a5d1f041c5ee64f5b9ec19ef
diwas <diwas.bhattarai@web.de>
2020-11-05 15:34:49 +0100

    imagezmq, cleanup

--- sub_client.py
+++ sub_client.py
@@ -1,20 +1,21 @@
-"""pub_sub_receive.py -- receive OpenCV stream using PUB SUB."""
-
 import sys
-
 import socket
 import traceback
 import cv2
-from imutils.video import VideoStream
 import imagezmq
 import threading
 import numpy as np
 import time
 from pupil_detectors import Detector2D, Detector3D
 
-detector = Detector2D()
 
 # Helper class implementing an IO deamon thread
+# When using the PUB/SUB pattern, the receiver of the frames will always receive all frames of the publisher. 
+# This works as long as the receiver can keep up with the incoming data. If receiver is slow it can fall behind 
+# and will not process the most recent frames from the publisher, but whatever is still in the receive queue of the zmq socket.
+#A better approach (if network bandwidth is not most concerning) is to keep the socket open, receive every 
+# frame in a dedicated IO thread, but only process the most recent one in a processing thread. 
+
 class VideoStreamSubscriber:
 
     def __init__(self, hostname, port):
@@ -36,9 +37,7 @@ class VideoStreamSubscriber:
 
     def _run(self):
         receiver = imagezmq.ImageHub("tcp://{}:{}".format(self.hostname, self.port), REQ_REP=False)
-        #print(receiver.zmq_socket.hwm)
-        #receiver.zmq_socket.set_hwm(1)
-        #print(receiver.zmq_socket.hwm)
+        receiver.zmq_socket.set_hwm(1)
         while not self._stop:
             self._data = receiver.recv_jpg()
             self._data_ready.set()
@@ -47,18 +46,13 @@ class VideoStreamSubscriber:
     def close(self):
         self._stop = True
 
-# Simulating heavy processing load
-def limit_to_2_fps():
-    time.sleep(0.5)
-
 if __name__ == "__main__":
     # Receive from broadcast
-    # There are 2 hostname styles; comment out the one you don't need
     #hostname = "127.0.0.1"  # Use to receive from localhost
     hostname = "192.168.0.137"  # Use to receive from other computer
     port = 5555
     receiver = VideoStreamSubscriber(hostname, port)
-
+    detector = Detector2D()
     try:
         start_time = time.time()
         x = 1
@@ -70,26 +64,26 @@ if __name__ == "__main__":
         lineType               = 1
         fps = 0
         while True:
+            # Due to the IO thread constantly fetching images, we can do any amount
+            # of processing here and the next call to receive() will still give us
+            # the most recent frame (more or less realtime behaviour)
+
             msg, frame = receiver.receive()
             receive_time = time.time()
             image = cv2.imdecode(np.frombuffer(frame, dtype='uint8'), -1)
             frame_time = msg
             latency = receive_time - frame_time
             print(latency)
-            #print(str(receive_time) + " " + str(frame_time) + "=" + str(latency))
-            # Due to the IO thread constantly fetching images, we can do any amount
-            # of processing here and the next call to receive() will still give us
-            # the most recent frame (more or less realtime behaviour)
 
-            # Uncomment this statement to simulate processing load
-            # limit_to_2_fps()   # Comment this statement out to run full speeed
-            
+            # Pupil detection happens here
             result = detector.detect(image)
             ellipse = result["ellipse"]
 
             # draw the ellipse outline onto the input image
             # note that cv2.ellipse() cannot deal with float values
             # also it expects the axes to be semi-axes (half the size)
+
+            # Convert it to rgb to display it
             image_rgb = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
             cv2.ellipse(
                 image,
